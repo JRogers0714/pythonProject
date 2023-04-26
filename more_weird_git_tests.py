fbb72a93d9c96f046d23e007bc2166ab72679f45
Joshua Rogers <Joshua.M.Rogers@Outlook.com>
2023-04-25 21:26:39 -0400

    asc v desc sorting/ git testing

---
+++ more_weird_git_tests.py
@@ -0,0 +1,14 @@
+# practicing different concepts and trying to figure out how to get these file to main instead of master.
+
+prime_numbers = [11, 3, 7, 5, 2]
+
+# sorting the list in ascending order
+prime_numbers.sort()
+
+print(prime_numbers)
+
+# Output: [2, 3, 5, 7, 11]
+
+prime_numbers.sort(reverse=True)
+
+print(prime_numbers)
