5844c378772148f3d5f4056f9f3a30abe4924108
Joshua Rogers <Joshua.M.Rogers@Outlook.com>
2023-04-25 21:05:19 -0400

    bubblesort/ git testing

---
+++ trying_Git_out.py
@@ -0,0 +1,25 @@
+
+
+# bubble sort
+
+def bubble_sort(arr):
+    n = len(arr)
+    swapped = False
+
+    for i in range(n - 1):
+        for j in range(0, n-i-1):
+
+            if arr[j] > arr[j + 1]:
+                swapped = True
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+        if not swapped:
+            return
+
+
+arr = [69, 420, 4, 44, 444, 14]
+
+print("Unsorted list is: ")
+print(arr)
+bubble_sort(arr)
+print("Sorted list is: ")
+print(arr)
