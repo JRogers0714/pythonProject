0f769ee2fa16d8a73a906c305c092acae9464325
Joshua Rogers <Joshua.M.Rogers@Outlook.com>
2023-04-28 10:30:54 -0400

    just getting started. saving work before I run these pycharm updates

--- pcep_literals_n_such.py
+++ pcep_literals_n_such.py
@@ -3,5 +3,17 @@ print(2)  # integer
 
 #  both are literals
 
+#  always use underscores in numeric literals
 
+# negative numbers can be represented with a negative signa nd positive a plus.
+
+
+print(-149_863)
+print(+69_420)  # plus sign is optional
+
+#  getting weird with octal and hexadecimals
+
+print(0o123)  # 0o makes this an octal value
+
+print(0x123)  # 0x makes this a hex value
 
