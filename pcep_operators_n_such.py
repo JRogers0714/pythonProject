3395a1154c1666a0f10ee4b26e28b2def9be9666
Joshua Rogers <Joshua.M.Rogers@Outlook.com>
2023-04-28 15:30:13 -0400

    getting used to saving progress as I go./ makign my way through this study guide. about to start addition.

--- pcep_operators_n_such.py
+++ pcep_operators_n_such.py
@@ -19,5 +19,24 @@ print(6. / 3.)
 
 # what if we want an integer?
 
+# // is always divisional conforms to float rule and rounds
+
+print(6 // 4)
+print(6. // 4)  # rounding always goes to the lesser integer
+
+print(-6 // 4)
+print(6. // -4)
+
+#  % is a remainder left after integer division; aka modulo
+
+print(14 % 4)  # the answer here is 2 because 4 goes into 14 3 times... with *2* left over
+
+print(12 % 4.5)
+
+
+#  division by 0 does not work.
+
+# addition
+
 
 
