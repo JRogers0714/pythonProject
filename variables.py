d63b10cad42df94b083288dbfca449371fa442e3
JRogers0714 <131318642+JRogers0714@users.noreply.github.com>
2023-05-05 11:37:57 -0400

    getting started

---
+++ variables.py
@@ -0,0 +1,21 @@
+#  ['False', 'None', 'True', 'and', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+
+#  the above characters have special meaning and should not be used as variable names
+
+var = 1
+print(var)
+
+var = 1
+account_balance = 1000.0
+client_name = 'John Doe'
+print(var, account_balance, client_name)
+print(var)
+
+var = "3.8.5"
+print("Python version: " + var)
+
+var = 1
+print(var)
+var = var + 1
+print(var)
+
