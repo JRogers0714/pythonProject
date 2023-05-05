c11b2e6ce8425e47b31bd98f491ff6bd67baf42e
JRogers0714 <131318642+JRogers0714@users.noreply.github.com>
2023-05-05 11:30:09 -0400

    finished another module

--- pcep_operators_n_such.py
+++ pcep_operators_n_such.py
@@ -49,3 +49,14 @@ print(+2) # unary + operator
 
 print(9 % 6 % 2)  # Binding to then left
 
+print(2 ** 2 ** 3)  # exponential right side binding
+
+print(2 * 3 % 5)
+
+print((5 * ((25 % 13) + 100) / (2 * 13)) // 2)
+
+print((2 ** 4), (2 * 4.), (2 * 4))
+
+print((-2 / 4), (2 / 4), (2 // 4), (-2 // 4))
+
+print((2 % -4), (2 % 4), (2 ** 3 ** 2))
