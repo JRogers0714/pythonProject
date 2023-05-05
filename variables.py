f033d8174ad5ca72ff94af5f540ab5d5e48cbbfe
JRogers0714 <131318642+JRogers0714@users.noreply.github.com>
2023-05-05 11:50:33 -0400

    playing with variables

--- variables.py
+++ variables.py
@@ -19,3 +19,35 @@ print(var)
 var = var + 1
 print(var)
 
+var = 100
+var = 200 + 300
+print(var)
+
+a = 3.0
+b = 4.0
+c = (a ** 2 + b ** 2) ** 0.5
+print("c =", c)
+
+# Variables lab
+
+john = 3
+
+mary = 5
+
+adam = 6
+
+print(john, mary, adam)
+
+print(john + mary + adam)
+
+
+def total_apples(a, b, c):
+
+    print(a + b + c)
+
+
+total_apples(john, mary, adam)
+
+all_apples = john + mary + adam
+
+print(all_apples)
